fda4dba8b768b55c9b7f93ac3bdfa3214cb948cd
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2024-12-21 18:50:03 +0530

    Revert "updated ui"
    This reverts commit f4ce1cf6b63756e20963f6cecab54b61ccfbba6a.

---
+++ src/app/app/page.tsx
@@ -0,0 +1,10 @@
+import App from "../components/App";
+import RootLayout from "../layout";
+
+export default function Home() {
+  return (
+    <RootLayout>
+      <App />
+    </RootLayout>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,6 @@
-"use client";
+import Image from "next/image";
+import App from "./components/App";
 
-import { Header } from "@/app/components/header/Header";
-import BackgroundRays from "@/app/components/ui/BackgroundRays";
-import { Chat } from "@/app/components/chat/Chat.client";
-
-export default function Page() {
-  return (
-    <div className="flex flex-col h-screen w-full bg-bolt-elements-background-depth-1">
-      <BackgroundRays />
-      <Header />
-      {/* Chat Component */}
-      <Chat />
-    </div>
-  );
+export default function Home() {
+  return <App />
 }
